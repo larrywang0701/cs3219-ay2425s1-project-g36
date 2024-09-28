02c5ed6d42a8988594dcb43e242e4b18258b3491
wjayee <wong.jayee@gmail.com>
2024-09-29 02:19:32 +0800

    Fix connection issues for 2 databases

--- user-service/db/connectDB.ts
+++ user-service/db/connectDB.ts
@@ -3,10 +3,12 @@ import { USER_SERVICE_MONGO_URI, BLIST_MONGO_URI } from '../utils/config';
 
 const connectMongoDB = async () => {
   try {
-      const user_conn: Mongoose = await mongoose.connect(USER_SERVICE_MONGO_URI);
-      const blist_conn: Mongoose = await mongoose.connect(BLIST_MONGO_URI);
-      console.log(`MongoDB connected: ${user_conn.connection.host}`);
-      console.log(`MongoDB connected: ${blist_conn.connection.host}`);
+    //   const user_conn: Mongoose = await mongoose.connect(USER_SERVICE_MONGO_URI);
+    //   const blist_conn: Mongoose = await mongoose.connect(BLIST_MONGO_URI);
+      const user_conn = await mongoose.createConnection(USER_SERVICE_MONGO_URI);
+      const blist_conn = await mongoose.createConnection(BLIST_MONGO_URI);
+      console.log(`MongoDB connected: ${user_conn.host}`);
+      console.log(`MongoDB connected: ${blist_conn.host}`);
   } catch (error) {
       if (error instanceof Error) {
           console.error(`Error connecting to MongoDB: ${error.message}`);
