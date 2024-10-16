f12b2348c8533cb80a4c95c2186c2bc4ac66a06c
tiongMax <maxtiongcs16@gmail.com>
2024-10-16 16:49:14 +0800

    Use 1 database for both user and blacklisted token

--- user-service/db/connectDB.ts
+++ user-service/db/connectDB.ts
@@ -1,15 +1,11 @@
 import mongoose, { Mongoose } from "mongoose";
 
-import { USER_SERVICE_MONGO_URI, BLIST_MONGO_URI } from '../utils/config';
+import { USER_SERVICE_MONGO_URI } from '../utils/config';
 
 const connectMongoDB = async () => {
   try {
-    //   const user_conn: Mongoose = await mongoose.connect(USER_SERVICE_MONGO_URI);
-    //   const blist_conn: Mongoose = await mongoose.connect(BLIST_MONGO_URI);
-      const user_conn = await mongoose.createConnection(USER_SERVICE_MONGO_URI);
-      const blist_conn = await mongoose.createConnection(BLIST_MONGO_URI);
-      console.log(`User DB connected: ${user_conn.readyState ? 'Connected' : 'Not connected'}`);
-      console.log(`Blacklist DB connected: ${blist_conn.readyState ? 'Connected' : 'Not connected'}`);
+      const user_conn: Mongoose = await mongoose.connect(USER_SERVICE_MONGO_URI);
+      console.log(`MongoDB connected: ${user_conn.connection.host}`);
   } catch (error) {
       if (error instanceof Error) {
           console.error(`Error connecting to MongoDB: ${error.message}`);
--- user-service/utils/config.ts
+++ user-service/utils/config.ts
@@ -6,10 +6,6 @@ if (!process.env.USER_SERVICE_MONGODB_URI) {
     throw new Error('USER_SERVICE_MONGODB_URI is not defined in the environment variables');
 }
 
-if (!process.env.BLIST_MONGODB_URI) {
-    throw new Error('BLIST_MONGODB_URI is not defined in the environment variables');
-}
-
 if (!process.env.EMAIL) {
     throw new Error('EMAIL is not defined in the environment variables');
 }
@@ -24,7 +20,6 @@ if (!process.env.JWT_SECRET) {
 
 export const PORT: string | number = process.env.PORT || 4000;
 export const USER_SERVICE_MONGO_URI = process.env.USER_SERVICE_MONGODB_URI;
-export const BLIST_MONGO_URI = process.env.BLIST_MONGODB_URI;
 export const EMAIL = process.env.EMAIL;
 export const PASSWORD = process.env.PASSWORD;
 export const JWT_SECRET = process.env.JWT_SECRET;
