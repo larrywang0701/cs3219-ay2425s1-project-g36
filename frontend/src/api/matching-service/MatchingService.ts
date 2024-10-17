e802af6df01b8dc06f0fbc152fa94df0f42a9bf0
larrywang0701 <113608053+larrywang0701@users.noreply.github.com>
2024-10-18 05:55:59 +0800

    Add documentation for matching service API functions

--- frontend/src/api/matching-service/MatchingService.ts
+++ frontend/src/api/matching-service/MatchingService.ts
@@ -14,6 +14,14 @@ const api = axios.create({baseURL : MATCHING_SERVICE_URL})
 
 let previousStartMatchingData : any = null;
 
+/**
+ * An async function for sending a start matching request to the backend.
+ * 
+ * @param token The current user's token.
+ * @param difficulties The difficulties selected by the user for matching.
+ * @param topics The topics selected by the user for matching.
+ * @returns An object containing the HTTP status code of the request and the message from the backend server
+ */
 async function sendStartMatchingRequest(token : string, difficulties : SelectedDifficultyData, topics : string[]) {
   const requestBody = {
     userToken : token,
@@ -30,6 +38,13 @@ async function sendStartMatchingRequest(token : string, difficulties : SelectedD
   })
 }
 
+/**
+ * An async function for resending the previous start matching request to the backend.
+ * This function will use the `difficulties` and `topics` data from the previous `sendStartMatchingRequest` call, so make sure that you have called `sendStartMatchingRequest` at least for one time before calling this function.
+ * 
+ * @param token The current user's token.
+ * @returns An object containing the HTTP status code of the request and the message from the backend server.
+ */
 async function retryPreviousMatching(token : string) {
   if(previousStartMatchingData === null) {
     throw new Error("[retryMatching] previousStartMatchingData is null");
@@ -42,6 +57,13 @@ async function retryPreviousMatching(token : string) {
   })
 }
 
+/**
+ * An async function for sending a check matching state request to the backend.
+ * You should call this function intermittently for multiple times when waiting for matching in order to get the latest matching state.
+ * 
+ * @param token The current user's token.
+ * @returns An object containing the HTTP status code of the request and the message from the backend server.
+ */
 async function sendCheckMatchingStateRequest(token : string) {
   const requestBody = {
     userToken : token
@@ -56,6 +78,13 @@ async function sendCheckMatchingStateRequest(token : string) {
   });
 }
 
+/**
+ * An async function for sending a cancel matching request to the backend.
+ * The request is for notifying the backend that the frontend stops using the matching service (for example: user decided to cancel matching, user left the waiting matching page, etc.)
+ * 
+ * @param token The current user's token.
+ * @returns An object containing the HTTP status code of the request and the message from the backend server.
+ */
 async function sendCancelMatchingRequest(token : string) {
   const requestBody = {
     userToken : token
@@ -67,6 +96,13 @@ async function sendCancelMatchingRequest(token : string) {
   })
 }
 
+/**
+ * An async function for updating the ready state of the current user to the backend.
+ * 
+ * @param token The current user's token.
+ * @param isReady `true` if the user is confirmed ready, and `false` if the user failed to get ready in time.
+ * @returns An object containing the HTTP status code of the request and the message from the backend server.
+ */
 async function sendUpdateReadyStateRequest(token : string, isReady : boolean) {
   const requestBody = {
     userToken : token
@@ -79,6 +115,13 @@ async function sendUpdateReadyStateRequest(token : string, isReady : boolean) {
   })
 }
 
+/**
+ * An async function for sending a check peer ready state request to the backend.
+ * You should call this function intermittently for multiple times when waiting for the peer to get ready in order to get the latest state.
+ * 
+ * @param token The current user's token.
+ * @returns An object containing the HTTP status code of the request and the message from the backend server.
+ */
 async function sendCheckPeerReadyStateRequest(token : string) {
   const requestBody = {
     userToken : token
