d95a487c20f6d1b855368fb390bd6439ac39a5db
Brendan Koh <brenstack62@gmail.com>
2024-10-28 15:12:43 +0800

    Update logout function in AuthContext to remove from matching queue

--- frontend/src/api/matching-service/MatchingService.ts
+++ frontend/src/api/matching-service/MatchingService.ts
@@ -18,7 +18,7 @@ let previousStartMatchingData : any = null;
 /**
  * An async function for sending a start matching request to the backend.
  * 
- * @param id The current user's id.
+ * @param id The current user's ID.
  * @param difficulties The difficulties selected by the user for matching.
  * @param topics The topics selected by the user for matching.
  * @returns An object containing the HTTP status code of the request and the message from the backend server
@@ -63,7 +63,7 @@ async function retryPreviousMatching(token : string) {
  * An async function for sending a check matching state request to the backend.
  * You should call this function intermittently for multiple times when waiting for matching in order to get the latest matching state.
  * 
- * @param id The current user's id.
+ * @param id The current user's ID.
  * @returns An object containing the HTTP status code of the request and the message from the backend server.
  */
 async function sendCheckMatchingStateRequest(id : string) {
@@ -84,7 +84,7 @@ async function sendCheckMatchingStateRequest(id : string) {
  * An async function for sending a cancel matching request to the backend.
  * The request is for notifying the backend that the frontend stops using the matching service (for example: user decided to cancel matching, user left the waiting matching page, etc.)
  * 
- * @param token The current user's token.
+ * @param id The current user's ID.
  * @returns An object containing the HTTP status code of the request and the message from the backend server.
  */
 async function sendCancelMatchingRequest(id : string) {
